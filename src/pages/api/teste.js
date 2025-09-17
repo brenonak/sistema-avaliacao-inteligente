07149b4f2d7439b00344c2094c08d90ee311b10d
Lucas Ferrara <lucascauaferrara@gmail.com>
2025-09-16 21:52:31 -0300

    configuração inicial do banco de dados

---
+++ src/pages/api/teste.js
@@ -0,0 +1,9 @@
+import clientPromise from "@/lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db();
+  const colecao = db.collection("exemplo");
+  const dados = await colecao.find({}).toArray();
+  res.json(dados);
+}
