a8fb01d7215828758346e4411efc1abc41652b48
fernandofelxs <fernando.felix@unifesp.br>
2025-11-11 13:39:09 -0300

    style: fazer os valores dos gráficos ficarem no intervalo [0, 100]

--- src/app/components/StudentPerformanceChart.jsx
+++ src/app/components/StudentPerformanceChart.jsx
@@ -13,7 +13,8 @@ export default function StudentPerformanceChart({ labels, scores, text, height =
         </Typography>
         <LineChart
           height={height}
-          xAxis={[{ data: labels, scaleType: 'point' }]}
+          xAxis={[{ data: labels, scaleType: 'point' }]} 
+          yAxis={[{ min: 0, max: 100, tickNumber: 6 }]}
           series={[{ data: scores, label: 'Nota (%)', color: theme.palette.accent?.main || theme.palette.primary.main }]}
           margin={{ left: 20, right: 50, top: 30, bottom: 40 }}
         />
